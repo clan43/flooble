e80a7c87f70611c0bc98f2543e40458f166ccfc3
srizon <96226829+clan43@users.noreply.github.com>
2022-03-26 22:03:12 +0600

    My Practice

---
+++ script.js
@@ -0,0 +1,48 @@
+// ###################### Write JavaScript Code ######################### 
+"use strict";
+
+console.log("ধৈর্য এমন একটি গাছ , যার সারা গায়ে কাটা কিন্তু ফল অতি সুস্বাদু ।");
+
+// ############################  First to last ##############################
+
+
+const numbers = [10,20,30,40];
+
+const result = numbers.map(number => number + number)
+
+console.log(result);
+
+
+
+
+
+
+// const numbers =  [4,8,5,6];
+
+// const myFunc =(number,index)=>{
+    
+//         console.log(`${index} == ${number*2}`);
+
+// }
+
+// numbers.forEach(myFunc);
+
+
+
+
+
+
+
+
+
+
+// let numbers;
+// let numbers1;
+
+// document.getElementById("myButton").onclick = ()=>{
+//         numbers =  document.getElementById("myText").value;
+//         numbers1 =  document.getElementById("myText1").value;
+//         let result = +numbers + +numbers1
+//         console.log(result);
+// }
+
